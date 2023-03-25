50fe63ced479a1b62c0bf23465d7e8907ce469e3
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-03-25 23:06:38 +0100

    update task 1
    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,24 +1,35 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
-
 const galeryListEl = document.querySelector(".gallery");
 
 const imgPreview = galleryItems.map((item) => item.preview);
 const imgOriginal = galleryItems.map((item) => item.original);
 const imgDescription = galleryItems.map((item) => item.description);
 
-console.log(imgOriginal);
-console.log(imgDescription);
-
 galleryItems.forEach(function (item, i) {
 	const li = document.createElement("li");
 	galeryListEl.append(li);
+	const div = document.createElement("div");
+	div.classList.add("gallery__item");
+	li.append(div);
+	const a = document.createElement("a");
+	a.classList.add("gallery__link");
+	a.setAttribute("href", imgOriginal[i]);
+	div.append(a);
 	const newImg = document.createElement("img");
-	li.append(newImg);
+	a.append(newImg);
 	item = newImg;
-	console.log(item);
+	newImg.classList.add("gallery__image");
 	newImg.setAttribute("src", imgPreview[i]);
 	newImg.setAttribute("alt", imgDescription[i]);
+	newImg.setAttribute("data-source", imgOriginal[i]);
+});
+
+galeryListEl.addEventListener("click", (e) => {
+	const instance = basicLightbox.create(`
+	<img src=${e.target.dataset.source}>
+	`);
+	console.log(e.target.dataset.source);
+	instance.show();
+	instance.preventDefault();
 });
