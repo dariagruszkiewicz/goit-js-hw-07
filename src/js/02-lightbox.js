bd45f1a76cb1f420ee84625b84dd799d891b1b32
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-03-26 21:52:06 +0200

    update task 2
    utworzenie galerii, podłączenie skryptów

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galeryListEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const imgPreview = galleryItems.map((item) => item.preview);
+const imgOriginal = galleryItems.map((item) => item.original);
+const imgDescription = galleryItems.map((item) => item.description);
+
+galleryItems.forEach(function (item, i) {
+	const li = document.createElement("li");
+	galeryListEl.append(li);
+	const a = document.createElement("a");
+	a.classList.add("gallery__item");
+	a.setAttribute("href", imgOriginal[i]);
+	li.append(a);
+	const newImg = document.createElement("img");
+	a.append(newImg);
+	item = newImg;
+	newImg.classList.add("gallery__image");
+	newImg.setAttribute("src", imgPreview[i]);
+	newImg.setAttribute("alt", imgDescription[i]);
+});
